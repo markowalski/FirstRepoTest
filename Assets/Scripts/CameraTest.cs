b696397d945028aaa67f92dad35d1b4fae6e4f03
Mariusz Kowalski <mariusz.grzegorz.kowalski@gmail.com>
2017-07-06 10:02:19 +0200

    First commit, some basic scripts and standard assets from asset store

---
+++ Assets/Scripts/CameraTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 pos = transform.position;
+        transform.position = new Vector3(pos.x + .01f, pos.y, pos.z);
+	}
+}
---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+	    	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 pos = transform.position;
+        transform.position = new Vector3(pos.x + Random.value * .1f, pos.y + Random.value * .1f, pos.z);
+    }
+}

7e7307d1fbc3125266e56200b0f6beced25fb613
Mariusz Kowalski <mariusz.grzegorz.kowalski@gmail.com>
2017-07-06 10:13:13 +0200

    New CameraTest variable

--- Assets/Scripts/CameraTest.cs
+++ Assets/Scripts/CameraTest.cs
@@ -12,7 +12,7 @@ public class CameraTest : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        //Vector3 pos = transform.position;
+        Vector3 pos = transform.position;
         //transform.position = new Vector3(pos.x + .01f, pos.y, pos.z);
 	}
 }
